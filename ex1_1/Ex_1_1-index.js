d533cf9ccfb4e996800f61b1fa9ff209347319e6
shhady <shhadyse@gmail.com>
2022-06-16 01:18:03 +0300

    1_1

---
+++ ex1_1/Ex_1_1-index.js
@@ -0,0 +1,38 @@
+const assert = require("assert");
+
+console.log(global);
+// console.log(global.console);
+// global.a2 = "hi";
+// console.log(global);
+
+console.log(global.process.versions);
+console.log(global.process.release);
+//curl -O https://node
+
+// console.log(assert);
+// assert(5 > 7);
+
+// console.log(Buffer);
+// The buffers module provides a way of handling streams of binary data.
+//converting a string to binary data
+var buf = Buffer.from("hello");
+console.log(buf);
+
+//client side js
+//window, document, alert,blur, fetch
+
+console.log(globalThis);
+
+console.log("hello");
+//------------------------------------------------------------------------
+//? Solution
+
+/// Browser ///
+// 1. document
+// 2.window
+// 3.fetch
+
+/// Node ///
+//1. global
+//2.process
+//3.Buffer
